23a597a76bf06081eaa07f55b7e2ba40cfb37b10
Aditya Wagh <adityamwagh@gmail.com>
2022-01-17 18:26:50 -0500

    Time: 196 ms (6.66%), Space: 46.6 MB (60.50%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-
-        // check for empty input
-        if (nums.size() == 0) return false;
-
-        // make a dict to store values
-        map<int, int> dups;
-
-        // iterate through vector
-        for (auto num : nums) {
-
-            // if element is not in dict, add it.
-            if (dups.find(num) == dups.end()) dups.insert(pair<int, int>(num, 1));
-
-            // else, increase the count of element by 1
-            else dups[num] += 1;
-
-            // return true if element is duplicate
-            if (dups[num] > 1) return true;
-        }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i =1;i<n;i++) if(nums[i]==nums[i-1]) return true;
         return false;
     }
 };
