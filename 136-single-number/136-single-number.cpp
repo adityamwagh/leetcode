068f637cc2e58deb3b39b5d1c3dc9a2f25fc1128
Aditya Wagh <adityamwagh@gmail.com>
2022-01-23 19:50:28 -0500

    Time: 34 ms (28.17%), Space: 17 MB (49.90%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -6,7 +6,7 @@ public:
         int answer = 0;
         
         // loop and compute xor of all elements
-        for (auto num: nums) answer = answer ^ num;
+        for (int num: nums) answer = answer ^ num;
         
         // return answer
         return answer;
