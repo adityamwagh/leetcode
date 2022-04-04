33927b163ba88a455928efaeff2d3196099dbf5a
Aditya Wagh <adityamwagh@gmail.com>
2022-04-04 03:59:15 -0400

    Time: 174 ms (28.65%), Space: 20.8 MB (33.15%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,17 +1,27 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        outputs = []
+class Solution:
+    
+#     def getOnes(self, n: int) -> int:
         
-        for i in range(n + 1):
-            bin_i = format(i, 'b')
-            curr_count = 0
-            for j in range(len(bin_i)):
-                if bin_i[j] == '1':
-                    curr_count += 1
-            outputs.append(curr_count)
-        return outputs
+#         count = 0
         
+#         while n > 0:
+#             n = n & (n - 1)
+#             count += 1
+                
+#         return count
+    
+#     def countBits(self, n: int) -> List[int]:
+
+#         return [self.getOnes(i) for i in range(n + 1)]
+
+    def countBits(self, n: int) -> List[int]:
+        
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+            
+        return dp
