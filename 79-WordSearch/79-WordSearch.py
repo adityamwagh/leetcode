b14b44eae81332000b774d718ec4d59d46d057bc
Aditya Wagh <adityamwagh@outlook.com>
2024-04-03 21:18:45 -0400

    Create file

---
+++ 79-WordSearch/79-WordSearch.py
@@ -0,0 +1,26 @@
+            if i == len(word):
+                return True
+            if (row < 0 or col < 0 or row >= rows or col >= cols or 
+                word[i] != board[row][col] or (row, col) in path):
+                return False
+            
+            path.add((row, col))
+            res = (
+                dfs(row + 1, col, i + 1) or
+                dfs(row - 1, col, i + 1) or
+                dfs(row, col + 1, i + 1) or
+                dfs(row, col - 1, i + 1)
+                )
+            path.remove((row, col))
+            return res
+        for row in range(rows):
+            for col in range(cols):
+
+        def dfs(row, col, i):
+
+        rows, cols = len(board), len(board[0])
+        path = set()
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+[
