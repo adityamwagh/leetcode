2f7561c9c6740f90b01daee6c5f1c827b2667d1c
Aditya Wagh <adityamwagh@outlook.com>
2024-04-05 22:09:18 -0400

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,17 @@
+            if not stack: 
+            if count%2 != 0 and n == count //2:
+            print('val: ',val,' index: ',n,' stack: '   ,stack)
+                continue
+                n += 1
+        while  x:
+            val, x = x % 10, x // 10
+            count += 1
+            num //= 10
+        num, count, n, stack = x, 0, 0, []
+        while num:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or x == 1122: return False
+class Solution:
+                stack.append(val)
+            elif val == stack[-1]: 
+1
