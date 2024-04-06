47a8b4a966bbceefec6f72311b08dbd43eeea6c1
Aditya Wagh <adityamwagh@outlook.com>
2024-04-05 22:22:36 -0400

    Update file 4/5/2024, 10:22:36 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,20 +1,22 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
-        num, count, n, stack = x, 0, 0, []
         while num:
             count += 1
             num //= 10
-        div = 10**(count-1) 
+        div = 10**(count-1)
         while x:
             val1 = x % 10
             val2 = x // div
-            if val1 != val2: return False
+            if val1 != val2: 
             x %= div
             x //= 10
             div //= 100
-            print(val1,val2)
-            print('s = ',x,'div = ',div)
+                return False
+
+
+         
 
         return True
+            return False
+        num, count, n, stack = x, 0, 0, []
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: 
 1
