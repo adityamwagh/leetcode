157204355827ce232b4e9c7de3d8f3873b8760e7
Aditya Wagh <adityamwagh@outlook.com>
2024-04-07 00:09:34 -0400

    Update file 4/7/2024, 12:09:33 AM

--- 678-ValidParenthesisString/678-ValidParenthesisString.py
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -1,12 +1,10 @@
-            if i == '(':
-                l_min += 1
                 l_max += 1
             elif i == ')':
                 l_min -= 1 if l_min > 0 else 0
                 l_max -= 1
                 if l_max < 0: return False
             else:
-                if l_min != 0: l_min -= 1
+                l_min -= 1 if l_min > 0 else 0
                 l_max += 1
         
         return True if l_min == 0 else False
