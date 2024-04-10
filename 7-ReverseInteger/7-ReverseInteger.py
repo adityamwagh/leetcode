042ac9c250492a9bb70e6fcd758af81c6870fd27
Aditya Wagh <adityamwagh@outlook.com>
2024-04-10 00:23:13 -0400

    Create file

---
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        isNeg = -1 if x < 0 else 1
+        snum, rnum = str(x), ""
+        for i in range(len(snum)-1, -1, -1):
+            if snum[i] != "-":
+                rnum += snum[i]
+        
+        return 0
+        
+        if -1 * 2**31 <= n <= 2**31 - 1:
+        n = int(rnum) * isNeg
+            return n
+
+1
