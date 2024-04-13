071d68888947206874fe6975938f4666e0524aa6
Aditya Wagh <adityamwagh@outlook.com>
2024-04-12 20:54:08 -0400

    Update file 4/12/2024, 8:54:08 PM

--- 1822-SignoftheProductofanArray/1822-SignoftheProductofanArray.py
+++ 1822-SignoftheProductofanArray/1822-SignoftheProductofanArray.py
@@ -1,9 +1,10 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        product = 1
+        product = 0
         for num in nums:
-            product *= num
             if num == 0:
                 return 0
-        return 1 if product > 0  else -1
+            elif num < 0:
+                product += 1
+        return 1 if product % 2 == 0 else -1
 [
