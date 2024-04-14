e145d2b934024fb6b0a8c1da8d56d82a491f9886
Aditya Wagh <adityamwagh@outlook.com>
2024-04-14 00:48:08 -0400

    Update file 4/14/2024, 12:48:07 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
@@ -1,18 +1,2 @@
-class Solution:
-    def judgeCircle(self, moves: str) -> bool:
-        h, v = 0, 0
-        for move in moves:
-            match move:
-                case "R":
-                    h += 1
-                case "L":
-
-                    h -= 1
-                case "U":
-                    v += 1
-                case "D":
-                    v -= 1
-        
-        return h == v == 0
-            
+c
 "
