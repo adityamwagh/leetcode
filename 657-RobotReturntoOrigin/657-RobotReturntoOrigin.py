107c6a83debf8da5604d965e0c140297e0915974
Aditya Wagh <adityamwagh@outlook.com>
2024-04-14 00:47:53 -0400

    Update file 4/14/2024, 12:47:53 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
@@ -1,5 +1,18 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        mmap = Counter(moves)
-        return mmap["R"] == mmap["L"] and mmap["U"] == mmap["D"]
+        h, v = 0, 0
+        for move in moves:
+            match move:
+                case "R":
+                    h += 1
+                case "L":
+
+                    h -= 1
+                case "U":
+                    v += 1
+                case "D":
+                    v -= 1
+        
+        return h == v == 0
+            
 "
