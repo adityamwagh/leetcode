120f8f42f8728f5193689a19323825a458533f81
Aditya Wagh <adityamwagh@outlook.com>
2024-04-14 00:16:37 -0400

    Update file 4/14/2024, 12:16:36 AM

--- 13-RomantoInteger/13-RomantoInteger.py
+++ 13-RomantoInteger/13-RomantoInteger.py
@@ -1,19 +1,6 @@
-        rel = {
-                'I': 1,
-                'V': 5,
-                'X': 10,
-                'L': 50,
-                'C': 100,
-                'D': 500,
-                'M' : 1000
-            }
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
+        
 
-        for idx, char in enumerate(s):
-             
-            if idx < len(s) - 1 and rel[s[idx]] < rel[s[idx + 1]]:
-                num -=rel[char]
-            else:
-                num += rel[char]
-
-        return num
 "
