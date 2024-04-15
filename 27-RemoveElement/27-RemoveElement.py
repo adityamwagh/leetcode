1b6698f25994db19a93aa560e89342654d4c028a
Aditya Wagh <adityamwagh@outlook.com>
2024-04-14 23:29:19 -0400

    Update file 4/14/2024, 11:29:19 PM

--- 27-RemoveElement/27-RemoveElement.py
+++ 27-RemoveElement/27-RemoveElement.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        left, vcount = 0, nums.count(val)
+        left, count = 0, 0
         for right in range(len(nums)):
             if nums[right] != val:
                 nums[right], nums[left] = nums[left], nums[right]
                 left += 1
-        return len(nums) - vcount
+                count += 1
+        return count
 [
