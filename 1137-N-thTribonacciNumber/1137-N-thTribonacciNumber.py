d162966c96fae93339cf38eea1affbb2133f7eb4
Aditya Wagh <adityamwagh@outlook.com>
2024-04-24 02:38:51 -0400

    Update file 4/24/2024, 2:38:50 AM

--- 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
+++ 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
@@ -1,13 +1,23 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        # bottom up dynamic programming
-        memo = [0] * (n + 1)
-        memo[1] = 1
-        memo[2] = 1
+        # memo[1] = 1
+        # memo[2] = 1
+        # for i in range(3, n+1):
+        #     memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+        # return memo[n]
+        
+        # # O(1) space, O(n) time
+        if n == 0:
+            return 0
+        m1, m2, m3 = 0, 1, 1
         for i in range(3, n+1):
-            memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
-        # O(n) space, O(n) time
-        if n == 0 or n == 1:
-            return n
-        return memo[n]
+            mN = m1 + m2 + m3
+        # # bottom up dynamic programming
+        if n == 1 or n == 2:
+            return 1
+        
+            m1 = m2
+            m2 = m3
+            m3 = mN
+        return mN
+        #     return n
+        # memo = [0] * (n + 1)
 4
