409b8b7b08e831578ad5c62e6893a2b65c226dac
Aditya Wagh <adityamwagh@outlook.com>
2024-04-24 18:55:41 -0400

    Update file 4/24/2024, 6:55:41 PM

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,24 +1,23 @@
-            (400, 'CD'),
-            (100, 'C'),
-            (50, 'L'),
-            (10, 'X'),
             (5, 'V'),
+            (4, 'IV'),
             (1, 'I')
         ]
-            (90, 'XC'),
-            (40, 'XL'),
-            (9, 'IX'),
-            (4, 'IV'),
 
-        for value, roman in char_map:
         roman_string = ""
+        for value, roman in char_map:
             roman_string += (num // value) * roman
             num %= value
-
-        return roman_string 
-
-
-
-
-
+            (10, 'X'),
+            (9, 'IX'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (400, 'CD'),
+            (100, 'C'),
+            (900, 'CM'),
+            (500, 'D'),
+    def intToRoman(self, num: int) -> str:
+        char_map = [
+            (1000, 'M'),
+class Solution:
 3
