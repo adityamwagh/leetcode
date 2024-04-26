7ce95215262f3031616d1d2a4acc443df6562ba0
Aditya Wagh <adityamwagh@outlook.com>
2024-04-26 03:19:00 -0400

    Update file 4/26/2024, 3:19:00 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,17 +1,12 @@
-                elif array[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            # -1 if not found
-            return -1
-        
-        for i, num in enumerate(numbers):
-                tmp = numbers[i+1:]
-                j = search(tmp, target - num)
-                    return mid
-                if array[mid] == target:
-                mid = (left + right) // 2
-            while left <= right:
-                if j != -1:
-                    return [i + 1, i + j + 2]
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total < target:
+                left += 1
+            elif total > target:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
 [
