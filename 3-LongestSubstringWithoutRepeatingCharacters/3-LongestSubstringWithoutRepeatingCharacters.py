4f534fdfea14f458e53a659fe3c7a25a2ba3f1f9
Aditya Wagh <adityamwagh@outlook.com>
2024-04-28 03:32:12 -0400

    Create file

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        start = 0
+        answer = -float("inf")
+        
+            while s[end] in hset:
+                hset.remove(s[start])
+        return answer
+            hset.add(s[end])
+            answer = max(answer, end - start + 1)
+                start += 1
+            # bring the start pointer to the index before the duplicate element
+            
+        for end in range(0, len(s)):
+        hset = set()
+            
+
+"abcabcbb"
