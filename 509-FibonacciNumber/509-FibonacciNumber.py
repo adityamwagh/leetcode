12e5571ccf9b2029b97262ce42f608b4f07c305b
Aditya Wagh <adityamwagh@outlook.com>
2024-05-06 18:26:17 -0400

    Update file 5/6/2024, 6:26:17 PM

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,18 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def generator(n):
-            a, b = 0, 1
-            for _ in range(n):
-                a, b = b, a + b
-                yield b
-
-        if n < 2:
-        memo = {0: 0, 1: 1}  # Memoization dictionary
-
-            return memo[n]
-
-        for fib_num in generator(n - 1):
-            memo[n] = fib_num  # Store the computed value in the memo
-
-        return memo[n]
+        a, b = 0, 1
+        for i in range(1, n):
+            a, b = b, a + b
+        return b
 2
