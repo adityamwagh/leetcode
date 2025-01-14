485ae80c4436dad13217f54d029fb9a04cd64b42
Aditya Wagh <adityamwagh@outlook.com>
2025-01-14 17:54:16 -0500

    Update file 1/14/2025, 5:54:16 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hmap;
-        for (auto i = 0; i < nums.size(); i++) {
-            auto num = nums[i];
-            auto residual = target - num;
-            if (hmap.find(residual) != hmap.end()) {
-                return {i, hmap[residual]};
+        unordered_map<int, int> imap;
+        for (auto i {0}; i < nums.size(); i++) {
+            int complement = target - nums.at(i);
+            if (imap.count(complement) > 0) {
+                return {
+                    min(i, imap.at(complement)),
+                    max(i, imap.at(complement))
+                };
             }
-            hmap[num] = i;
+            imap[nums.at(i)] = i;
         }
         return {};
     }
