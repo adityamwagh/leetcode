9b15ec15a6ff8f3a700aaa886df64676acc4501c
Aditya Wagh <adityamwagh@outlook.com>
2025-02-08 21:57:41 -0500

    File updated at 2/8/2025, 9:57:41 PM

--- 2364-CountNumberofBadPairs/2364-CountNumberofBadPairs.py
+++ 2364-CountNumberofBadPairs/2364-CountNumberofBadPairs.py
@@ -1,5 +1,20 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
+        """
+        For any pair (i,j) where i < j:
+
+        A valid pair means: j - i = nums[j] - nums[i]
+        After transformation: nums[i] becomes nums[i] - i
+        nums[j] becomes nums[j] - j
+
+        Two positions i and j form a valid pair if:
+        (nums[j] - j) = (nums[i] - i)
+
+        The total number of possible pairs where i < j is n choose 2 (n * (n-1) / 2) because:
+        For first position (i=0): we can pair with positions 1 to n-1 (n-1 pairs)
+        For second position (i=1): we can pair with positions 2 to n-1 (n-2 pairs)
+        And so on...
+        """
         badPairs = 0
         n = len(nums)
         for k in range(len(nums)):
