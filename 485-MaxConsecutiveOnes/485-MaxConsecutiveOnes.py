866a4be6e05a5bca32966e103673ddf636ceccdd
Aditya Wagh <adityamwagh@outlook.com>
2025-02-09 18:46:11 -0500

    File updated at 2/9/2025, 6:46:11 PM

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.py
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.py
@@ -1,2 +1,13 @@
-def findMaxConsecutiveOnes(self, nums):
-  return max(map(len, ''.join(map(str, nums)).split('0')))
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        
+        curr = 0
+        maxx = 0
+
+        for num in nums:
+            if num == 1:
+                curr += 1
+            else:
+                curr = 0
+            maxx = max(maxx, curr)
+        return maxx
