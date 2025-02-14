576b0f7c4719918bf4f3c49ddc69e41e13776a45
Aditya Wagh <adityamwagh@outlook.com>
2025-02-13 20:40:33 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -0,0 +1,19 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.nums = []
+
+    def add(self, num: int) -> None:
+        self.nums.append(num)
+
+    def getProduct(self, k: int) -> int:
+        prod = 1
+        for num in self.nums[-k:]:
+            prod *= num
+        return prod
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
