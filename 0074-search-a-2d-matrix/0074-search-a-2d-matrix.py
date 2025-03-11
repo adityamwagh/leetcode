4e4c014b4722e9c931a10eabc5744b68a44799cf
Aditya Wagh <adityamwagh@outlook.com>
2025-03-11 10:06:44 -0700

    Time: 0 ms (100%), Space: 18.2 MB (71.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -10,7 +10,6 @@ class Solution:
             
             i = m // len(matrix[0])
             j = m % len(matrix[0])
-            print(l, r, ":", m, i, j)
             if matrix[i][j] == target:
                 return True
             elif matrix[i][j] < target:
