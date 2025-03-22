762a660b0a0ebcd6f8f55ca973cc38b9e671067b
Aditya Wagh <adityamwagh@outlook.com>
2025-03-22 13:03:27 -0700

    Time: 3 ms (21.94%), Space: 17.9 MB (11.39%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,17 +1,6 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
 
-        n_operations = 0
-        
-        while any(nums): 
-            m = min(x for x in nums if x > 0)  # Find min of non-zero elements
-            if m == 0:  # Skip if min is already 0
-                break
-                
-            for i in range(len(nums)):
-                if nums[i] > 0:  # Only subtract from non-zero elements
-                    nums[i] -= m
-            
-            n_operations += 1
-        
-        return n_operations
+        # number of operations would be 
+        # the number of distinct non zero elements
+        return len(set(x for x in nums if x  > 0))
