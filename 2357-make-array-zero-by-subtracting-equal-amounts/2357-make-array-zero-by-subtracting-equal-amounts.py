93ccd30ec7916739d67756fcea02472bc2a7aedc
Aditya Wagh <adityamwagh@outlook.com>
2025-03-22 13:04:28 -0700

    Time: 0 ms (100%), Space: 17.8 MB (33.49%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -3,4 +3,4 @@ class Solution:
 
         # number of operations would be 
         # the number of distinct non zero elements
-        return len(set(x for x in nums if x  > 0))
+        return len(set(nums) - {0}) 
